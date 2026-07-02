6d1e2e2431dd33a520fe7ca9db62b9a685db8e22
Gokul SAJEEV <gokulpsajeev@gmail.com>
2026-07-02 10:21:31 +0530

    Lesson 4 - Tuples

---
+++ basics/lesson04_tuples.py
@@ -0,0 +1,19 @@
+# Lesson 4 - Tuples
+
+# Create a tuple of aircraft engines
+engines = ("GE9X", "Trent XWB", "PW1100G", "CFM LEAP")
+
+# Print the entire tuple
+print("Aircraft Engines:", engines)
+
+# Print the first engine
+print("First Engine:", engines[0])
+
+# Print the last engine
+print("Last Engine:", engines[-1])
+
+# Print the total number of engines
+print("Number of Engines:", len(engines))
+
+# Display the data type
+print("Data Type:", type(engines))
