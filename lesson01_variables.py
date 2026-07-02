c80cea830fc8cb7b9a54939917e6ee3dbb359eeb
Gokul SAJEEV <gokulpsajeev@gmail.com>
2026-07-02 10:12:37 +0530

    Lesson 1 - Variables

---
+++ lesson01_variables.py
@@ -0,0 +1,9 @@
+# Lesson 1 - Variables
+
+aircraft = "Airbus A350"
+speed = 905
+altitude = 35000
+
+print("Aircraft:", aircraft)
+print("Cruising Speed:", speed, "km/h")
+print("Altitude:", altitude, "ft")
